37684e067c163e277a3367c808807f22d13e223e
Siddharth Shah <sidshah895@gmail.com>
2025-03-20 00:45:07 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" element={<div>about page</div>} />
-        <Route path="/contact" element={<div>contact page</div>} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
